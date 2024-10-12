c91609770d2d3a17d57e2b15d9affcb16e439ed0
Akshay Trikha <akshaytrikha@gmail.com>
2024-10-12 14:45:21 -0700

    scrape MVP

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/scrape', methods=['POST'])
+def scrape():
+    # Get the URL from the form
+    url = request.form.get('url')
+    
+    # Call the function from scrape.py
+    result = scrape_function(url)
+    
+    # Return some response (you can customize this as needed)
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
